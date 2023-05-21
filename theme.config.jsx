c9333b75e0fd5dee3e41621feedf11ee1e569ae0
Nikita Barskov <nbarskov@gmail.com>
2023-05-21 21:08:41 +0000

    Add `docsRepositoryBase` to `theme.config.jsx` (#2)

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
     logo: <span>Things I do</span>,
+    docsRepositoryBase: "https://github.com/nikitabarskov/experience/blob/main",
     project: {
         link: 'https://github.com/nikitabarskov/experience'
     }
-    // ...
 }
