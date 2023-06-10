a34c42339d19d1da161a95878bcdc50237be1a67
Nikita Barskov <nbarskov@gmail.com>
2023-06-10 15:23:34 +0200

    Simplify `_app.mdx`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  defaultShowCopyCode: true,
 })
 
 module.exports = withNextra()
